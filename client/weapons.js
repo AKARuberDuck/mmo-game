031bac256c7f81ce3bfc8b700ccb305abd3aa0e4
AKARuberDuck <ss2875356@gmail.com>
2025-06-14 09:30:00 -0500

    Create weapons.js

---
+++ client/weapons.js
@@ -0,0 +1,23 @@
+export const weapons = {
+  rifle: {
+    name: 'Rifle',
+    damage: 20,
+    fireRate: 300,
+    maxAmmo: 10,
+    reloadTime: 1000
+  },
+  shotgun: {
+    name: 'Shotgun',
+    damage: 35,
+    fireRate: 700,
+    maxAmmo: 4,
+    reloadTime: 1500
+  },
+  pistol: {
+    name: 'Pistol',
+    damage: 10,
+    fireRate: 150,
+    maxAmmo: 15,
+    reloadTime: 800
+  }
+};
